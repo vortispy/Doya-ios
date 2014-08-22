bfa69ca83c2585d9467fa3e1822345b3997d8343
vortispy <vortispy@vortispy-no-MacBook-Pro.local>
2014-08-23 01:47:12 +0900

    rankView gets each cell

---
+++ Doya/RankCell.swift
@@ -0,0 +1,32 @@
+//
+//  RankCell.swift
+//  Doya
+//
+//  Created by vortispy on 2014/08/22.
+//  Copyright (c) 2014年 vortispy. All rights reserved.
+//
+
+import UIKit
+
+class RankCell: UITableViewCell {
+    @IBOutlet weak var rankPointLabel: UILabel!
+    @IBOutlet weak var rankIcon: UIImageView!
+    @IBOutlet weak var pictureView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func configureCell(rank: String){
+        self.rankPointLabel.text = rank
+        self.rankIcon.image = UIImage(named: rank)
+        self.pictureView.image = nil
+    }
+}
---
+++ Doya/RankingTableView.swift
@@ -0,0 +1,22 @@
+//
+//  RankingTableView.swift
+//  Doya
+//
+//  Created by vortispy on 2014/08/23.
+//  Copyright (c) 2014年 vortispy. All rights reserved.
+//
+
+import UIKit
+
+class RankingTableView: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
