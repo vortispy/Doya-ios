2b4c46b8efcdfd569d30ccea1788846723ad3fa5
vortispy <vortispy@vortispy-no-MacBook-Pro.local>
2014-08-23 15:35:38 +0900

    make DoyaData

---
+++ Doya/DoyaData.swift
@@ -0,0 +1,17 @@
+//
+//  DoyaData.swift
+//  Doya
+//
+//  Created by vortispy on 2014/08/23.
+//  Copyright (c) 2014年 vortispy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DoyaData{
+    var keyName: String!
+    var point: Int!
+    var rank: Int!
+    var image: UIImage!
+}
