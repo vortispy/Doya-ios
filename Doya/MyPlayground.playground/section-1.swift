26b288dbc9da2fab0b640010b08b87aa469c5727
vortispy <vortispy@vortispy-no-MacBook-Pro.local>
2014-10-08 16:54:52 +0900

    set multi size laucher icon

--- Doya/MyPlayground.playground/section-1.swift
+++ Doya/MyPlayground.playground/section-1.swift
@@ -3,3 +3,16 @@
 import UIKit
 
 var str = "Hello, playground"
+
+func str(s: String?) ->String?{
+    if let mystr = s{
+        return s
+    }
+    return nil
+}
+
+var foo: String!
+foo = "ho"
+foo.toInt()
+
+
